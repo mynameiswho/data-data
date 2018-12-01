ffedb286c7e7d82c5616583d1129afb04155b699
cptnikman <45437347+cptnikman@users.noreply.github.com>
2018-12-01 16:34:35 +0100

    Update stationary.py
    plots

--- stationary.py
+++ stationary.py
@@ -1,10 +1,22 @@
 
 import pandas as pd
-from statsmodels.tsa.stattools import adfuller
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+from statsmodels.tsa.stattools import adfuller
 
 #Inspired by
 #https://www.analyticsvidhya.com/blog/2016/02/time-series-forecasting-codes-python/
+
+def RollingMeanStdPlot(timeseries, period):
+    rollmean = df.rolling(period).mean()
+    rollstd = df.rolling(period).std()
+
+    plt.plot(timeseries, label = 'Original')
+    plt.plot(rollmean, label = 'Rolling mean')
+    plt.plot(rollstd, label = 'Rolling std')
+    plt.legend()
+    plt.show()
 
 #Removes trend and seasonality using decomposition
 def make_stationary(timeseries):
@@ -16,9 +28,9 @@ def make_stationary(timeseries):
 #Returns p-value of augmented Dickey-Fuller unit root test
 def check_stationary(timeseries, variable):
     timeseries = timeseries.dropna()
-    dftest2 = adfuller(timeseries[variable].values)[1]
+    dftest = adfuller(timeseries[variable].values)[1]
 
-    return dftest2
+    return dftest
 
 #TESTCODE
 
@@ -32,5 +44,8 @@ df = df.sort_values('DATE')
 
 #Initial plot
 print("Initial p-value: ", check_stationary(df,'VALUE'))
+RollingMeanStdPlot(df, 12)
+
 df = make_stationary(df)
 print("After making stationary:", check_stationary(df,'VALUE'))
+RollingMeanStdPlot(df, 12)
